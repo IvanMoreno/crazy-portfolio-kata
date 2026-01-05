092f2ee05016322f8103c8efaccbd4fa30e073f5
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-01-05 05:33:55 +0100

    refactor: extract display value

--- c_sharp/Portfolio/Portfolio.cs
+++ c_sharp/Portfolio/Portfolio.cs
@@ -118,6 +118,10 @@ public class Portfolio
             portfolioValue = new MeasurableValue(portfolioValue.Get() + asset.Value.Get());
         }
 
+        Display(portfolioValue);
+    }
+
+    void Display(MeasurableValue portfolioValue) {
         Console.WriteLine(portfolioValue);
     }
 }
