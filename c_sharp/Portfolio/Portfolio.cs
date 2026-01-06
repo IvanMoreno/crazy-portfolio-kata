10fbab024009a17f6a94c4e07638b3b4debeec39
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-01-06 21:22:46 +0100

    refactor: extract csv portfolio loader

--- c_sharp/Portfolio/Portfolio.cs
+++ c_sharp/Portfolio/Portfolio.cs
@@ -2,20 +2,39 @@ using static System.Globalization.CultureInfo;
 
 namespace Portfolio;
 
+public class CsvPortfolioLoader {
+    readonly string _portfolioCsvPath;
+
+    public CsvPortfolioLoader(string portfolioCsvPath) {
+        _portfolioCsvPath = portfolioCsvPath;
+    }
+
+    public IEnumerable<Asset> GetAllAssets() {
+        var readText = File.ReadAllText(_portfolioCsvPath);
+        var lines = readText.Split(Environment.NewLine);
+        return lines.Select(Parse);
+    }
+
+    static Asset Parse(string line) {
+        var columns = line.Split(",");
+        return new Asset(columns[0], DateTime.Parse(columns[1], CurrentCulture), new AssetValue(int.Parse(columns[2])));
+    }
+}
+
 public class Portfolio
 {
-    private readonly string _portfolioCsvPath;
     private readonly PortfolioDisplay _portfolioDisplay;
+    readonly CsvPortfolioLoader csvPortfolioLoader;
 
     public Portfolio(string portfolioCsvPath, PortfolioDisplay portfolioDisplay)
     {
-        _portfolioCsvPath = portfolioCsvPath;
+        csvPortfolioLoader = new CsvPortfolioLoader(portfolioCsvPath);
         _portfolioDisplay = portfolioDisplay;
     }
     
     public void ComputePortfolioValue()
     {
-        var allAssets = GetAllAssets();
+        var allAssets = csvPortfolioLoader.GetAllAssets();
         if (allAssets.Any(IsUnicorn)) {
             _portfolioDisplay.DisplayUnicorn(allAssets.First(IsUnicorn));
         }
@@ -28,16 +47,5 @@ public class Portfolio
 
     static AssetValue GetPortfolioValue(IEnumerable<Asset> allAssets) {
         return allAssets.Aggregate(new AssetValue(0), (current, asset) => current.Add(asset.GetValue(DateTime.Now)));
-    }
-
-    IEnumerable<Asset> GetAllAssets() {
-        var readText = File.ReadAllText(_portfolioCsvPath);
-        var lines = readText.Split(Environment.NewLine);
-        return lines.Select(Parse);
-    }
-
-    static Asset Parse(string line) {
-        var columns = line.Split(",");
-        return new Asset(columns[0], DateTime.Parse(columns[1], CurrentCulture), new AssetValue(int.Parse(columns[2])));
     }
 }
