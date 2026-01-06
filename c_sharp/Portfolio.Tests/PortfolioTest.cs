a63ab41d1ddfa1c9b5cd9aca60322dbfdc3f1251
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-01-06 21:35:20 +0100

    refactor: parameterize clock dependency

--- c_sharp/Portfolio.Tests/PortfolioTest.cs
+++ c_sharp/Portfolio.Tests/PortfolioTest.cs
@@ -21,7 +21,7 @@ public class PortfolioTest
     public void ComputeValue()
     {
         var spyDisplay = new SpyDisplay();
-        var app = new Portfolio(spyDisplay, new CsvAssetsLoader("../../../portfolio_no_unicorn.csv"));
+        var app = new Portfolio(spyDisplay, new CsvAssetsLoader("../../../portfolio_no_unicorn.csv"), new RealtimeClock());
 
         app.ComputePortfolioValue();
 
@@ -31,7 +31,7 @@ public class PortfolioTest
     [Test]
     public void ComputeValue_FailsWhenUnicornAppears() {
         var spyDisplay = new SpyDisplay();
-        var app = new Portfolio(spyDisplay, new CsvAssetsLoader("../../../portfolio.csv"));
+        var app = new Portfolio(spyDisplay, new CsvAssetsLoader("../../../portfolio.csv"), new RealtimeClock());
 
         app.ComputePortfolioValue();
 
--- c_sharp/Portfolio/Portfolio.cs
+++ c_sharp/Portfolio/Portfolio.cs
@@ -4,18 +4,18 @@ public class Portfolio
 {
     private readonly PortfolioDisplay _display;
     readonly CsvAssetsLoader _assetsLoader;
-    readonly RealtimeClock realtimeClock;
+    readonly RealtimeClock _clock;
 
-    public Portfolio(PortfolioDisplay display, CsvAssetsLoader assetsLoader)
+    public Portfolio(PortfolioDisplay display, CsvAssetsLoader assetsLoader, RealtimeClock clock)
     {
         _assetsLoader = assetsLoader;
         _display = display;
-        realtimeClock = new RealtimeClock();
+        _clock = clock;
     }
     
     public void ComputePortfolioValue()
     {
-        var assets = _assetsLoader.GetAllAssets(realtimeClock.GetTime());
+        var assets = _assetsLoader.GetAllAssets(_clock.GetTime());
         if (assets.ContainsUnicorn) {
             _display.ShowUnicorn(assets.GetFirstUnicorn());
         }
