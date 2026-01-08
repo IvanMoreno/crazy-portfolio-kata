9be77b53b03e1c0c669ad5584fbc46d599f274c6
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-01-08 05:32:52 +0100

    test: introduce mock display notion

--- c_sharp/Portfolio.Tests/PortfolioTest.cs
+++ c_sharp/Portfolio.Tests/PortfolioTest.cs
@@ -2,12 +2,21 @@ using NUnit.Framework;
 
 namespace Portfolio.Tests;
 
+public class MockDisplay : Display {
+    public void ShowPortfolio(MeasurableValue portfolioValue) {
+        throw new NotImplementedException();
+    }
+    public void ShowUnicorn(Asset asset) {
+        throw new NotImplementedException();
+    }
+}
+
 public class PortfolioTest
 {
     [Test]
-    public void Fix_Me()
-    {
-        var app = new Portfolio("../../../portfolio.csv", new LogDisplay());
+    public void Fix_Me() {
+        var display = new MockDisplay();
+        var app = new Portfolio("../../../portfolio.csv", display);
 
         app.ComputePortfolioValue();
 
