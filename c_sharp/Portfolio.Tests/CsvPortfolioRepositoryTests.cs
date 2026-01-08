043f53f0a1a6ac535fa1a965ea2e4fd3c93d7ec2
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-01-08 05:49:27 +0100

    test: get all assets from csv

---
+++ c_sharp/Portfolio.Tests/CsvPortfolioRepositoryTests.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace Portfolio.Tests;
+
+public class CsvPortfolioRepositoryTests {
+    [Test]
+    public void GetAllAssets() {
+        var repository = new CsvPortfolioRepository("../../../portfolio.csv");
+
+        var assets = repository.GetAssets();
+        
+        Assert.That(assets, Is.Not.Null);
+        Assert.That(assets.Count(), Is.EqualTo(3));
+    }
+}
