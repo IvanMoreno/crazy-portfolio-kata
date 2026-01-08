861fb1d4d4db34f3256a9f0035f17c7ec3d56f82
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-01-08 06:19:09 +0100

    refactor: inline method

--- c_sharp/Portfolio/Portfolio.cs
+++ c_sharp/Portfolio/Portfolio.cs
@@ -21,7 +21,77 @@ public class Portfolio
                 return;
             }
 
-            GetValue(asset, now);
+            if (asset.Date.Subtract(now).TotalDays < 0)
+            {
+                if (asset.Description != "French Wine")
+                {
+                    if (asset.Description != "Lottery Prediction")
+                    {
+                        if (asset.Value.Get() > 0)
+                        {
+                            if (asset.Description != "Unicorn")
+                            {
+                                asset.Value = new MeasurableValue(asset.Value.Get() - 20);
+                            }
+                            else {
+                                _display.ShowUnicorn(asset);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        asset.Value = new MeasurableValue(asset.Value.Get() - asset.Value.Get());
+                    }
+                }
+                else
+                {
+                    if (asset.Value.Get() < 200) asset.Value = new MeasurableValue(asset.Value.Get() + 20);
+                }
+            }
+            else
+            {
+                if (asset.Description != "French Wine" && asset.Description != "Lottery Prediction")
+                {
+                    if (asset.Value.Get() > 0.0)
+                    {
+                        if (asset.Description != "Unicorn")
+                        {
+                            asset.Value = new MeasurableValue(asset.Value.Get() - 10);
+                        }
+                        else {
+                            _display.ShowUnicorn(asset);
+                        }
+                    }
+                    else
+                    {
+                        if (asset.Description == "Unicorn") {
+                            _display.ShowUnicorn(asset);
+                        }
+                    }
+                }
+                else
+                {
+                    if (asset.Description == "Lottery Prediction")
+                    {
+                        if (asset.Value.Get() < 800)
+                        {
+                            asset.Value = new MeasurableValue(asset.Value.Get() + 5);
+
+                            if (asset.Date.Subtract(now).TotalDays < 11)
+                                if (asset.Value.Get() < 800)
+                                    asset.Value = new MeasurableValue(asset.Value.Get() + 20);
+
+                            if (asset.Date.Subtract(now).TotalDays < 6)
+                                if (asset.Value.Get() < 800)
+                                    asset.Value = new MeasurableValue(asset.Value.Get() + 100);
+                        }
+                    }
+                    else
+                    {
+                        if (asset.Value.Get() < 200) asset.Value = new MeasurableValue(asset.Value.Get() + 10);
+                    }
+                }
+            }
 
             portfolioValue = new MeasurableValue(portfolioValue.Get() + asset.Value.Get());
         }
@@ -29,83 +99,6 @@ public class Portfolio
         _display.ShowPortfolio(portfolioValue);
     }
 
-    void GetValue(Asset asset, DateTime now) {
-        if (asset.Date.Subtract(now).TotalDays < 0)
-        {
-            if (asset.Description != "French Wine")
-            {
-                if (asset.Description != "Lottery Prediction")
-                {
-                    if (asset.Value.Get() > 0)
-                    {
-                        if (asset.Description != "Unicorn")
-                        {
-                            asset.Value = new MeasurableValue(asset.Value.Get() - 20);
-                        }
-                        else {
-                            _display.ShowUnicorn(asset);
-                            return;
-                        }
-                    }
-                }
-                else
-                {
-                    asset.Value = new MeasurableValue(asset.Value.Get() - asset.Value.Get());
-                }
-            }
-            else
-            {
-                if (asset.Value.Get() < 200) asset.Value = new MeasurableValue(asset.Value.Get() + 20);
-            }
-        }
-        else
-        {
-            if (asset.Description != "French Wine" && asset.Description != "Lottery Prediction")
-            {
-                if (asset.Value.Get() > 0.0)
-                {
-                    if (asset.Description != "Unicorn")
-                    {
-                        asset.Value = new MeasurableValue(asset.Value.Get() - 10);
-                    }
-                    else {
-                        _display.ShowUnicorn(asset);
-                        return;
-                    }
-                }
-                else
-                {
-                    if (asset.Description == "Unicorn") {
-                        _display.ShowUnicorn(asset);
-                        return;
-                    }
-                }
-            }
-            else
-            {
-                if (asset.Description == "Lottery Prediction")
-                {
-                    if (asset.Value.Get() < 800)
-                    {
-                        asset.Value = new MeasurableValue(asset.Value.Get() + 5);
-
-                        if (asset.Date.Subtract(now).TotalDays < 11)
-                            if (asset.Value.Get() < 800)
-                                asset.Value = new MeasurableValue(asset.Value.Get() + 20);
-
-                        if (asset.Date.Subtract(now).TotalDays < 6)
-                            if (asset.Value.Get() < 800)
-                                asset.Value = new MeasurableValue(asset.Value.Get() + 100);
-                    }
-                }
-                else
-                {
-                    if (asset.Value.Get() < 200) asset.Value = new MeasurableValue(asset.Value.Get() + 10);
-                }
-            }
-        }
-    }
-    
     bool IsUnicorn(Asset asset, DateTime now) {
         if (asset.Description != "Unicorn") 
             return false;
